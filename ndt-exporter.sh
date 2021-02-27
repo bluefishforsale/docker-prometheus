ca3874fb5701ccfd47284a2ba8c1199488897872
terrac <terracnosaur@gmail.com>
2021-02-27 19:19:51 +0000

    ndt start script

---
+++ ndt-exporter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SERVICE="ndt-exporter"
+IMAGE="bluefishforsale/ndt-speedtest-exporter"
+VERSION="latest"
+
+docker rm -f ${SERVICE}
+
+sudo docker run -d \
+  -p 9140:9140 \
+  --dns 192.168.1.1 \
+  --dns-search local \
+  --restart=always \
+  --name=${SERVICE} \
+  --hostname=${HOSTNAME} \
+  ${IMAGE}:${VERSION}
